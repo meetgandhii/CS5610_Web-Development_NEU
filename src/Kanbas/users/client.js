61a249c8918d1711ac12c5f2b348f0b449f7fa8d
Meet Gandhi <meetgandhi@Meets-MacBook-Air.local>
2023-12-04 12:32:43 -0500

    a6 server update

--- src/Kanbas/users/client.js
+++ src/Kanbas/users/client.js
@@ -3,7 +3,7 @@ const request = axios.create({
     withCredentials: true,
   });
   
-export const BASE_API = "http://localhost:4000";
+export const BASE_API = "https://kanbas-server-t70p.onrender.com/";
 export const USERS_API = `${BASE_API}/api/users`;
 export const signin = async (credentials) => {
     const response = await request.post(`${USERS_API}/signin`, credentials);
